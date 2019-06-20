66085f6ed9372a8bcaef456d681f5ab5f799f56f
Sitthinon Chanaritthichai <sitthinon.c@hotmail.com>
2019-06-20 12:53:37 +0700

    init project

---
+++ DotNetMVC5Demo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DotNetMVC5Demo.Startup))]
+namespace DotNetMVC5Demo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
